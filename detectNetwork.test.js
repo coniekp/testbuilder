36abcd71b1499636d50372c466b58a72a6ac9536
Kony Pham <kpham2@alumni.nd.edu>
2018-05-08 14:20:57 -0500

    "Add test messages to MasterCard tests"

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -90,11 +90,11 @@ describe('Visa', function() {
   });
 
   it('has a prefix of 4 and a length of 16', function() {
-    assert(detectNetwork('4123456789012345'),'Visa');
+    assert.equal(detectNetwork('4123456789012345'),'Visa');
   });
 
   it('has a prefix of 4 and a length of 19', function() {
-    assert(detectNetwork('4123456789012345678'), 'Visa');
+    assert.equal(detectNetwork('4123456789012345678'), 'Visa');
   });
 });
 
@@ -105,15 +105,15 @@ describe('MasterCard', function() {
   //   http://chaijs.com/api/bdd/
   var expect = chai.expect;
  
-  it(FILL_ME_IN, function() {
+  it('has a prefix fo 51 and a length or 16', function() {
     expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
   });
  
-  it(FILL_ME_IN, function() {
+  it('has a prefix fo 52 and a length or 16', function() {
     expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
   });
  
-  it(FILL_ME_IN, function() {
+  it('has a prefix fo 53 and a length or 16', function() {
     expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
   });
  
